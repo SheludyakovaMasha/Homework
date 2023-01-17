6a4ad72dbceec91e2dda5cd45b9dc584971a3363
SheludyakovaMasha <113375463+SheludyakovaMasha@users.noreply.github.com>
2023-01-17 22:47:45 +0300

    Initial commit

---
+++ header.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Masha Sheludyakova
+//
+
+#ifndef MAIN_CPP_HEADER_HPP
+#define MAIN_CPP_HEADER_HPP
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+void encryption(const std::string &file_name, unsigned int key);
+
+void decryption(const std::string &file_name, unsigned int key);
+
+#endif //MAIN_CPP_HEADER_HPP
